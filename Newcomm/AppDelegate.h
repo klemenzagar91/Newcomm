a90ebbe776e686a2e020e7ba58d1cf2dce686d35
Klemen Zagar <klemen.zagar91@gmail.com>
2014-03-26 17:23:53 +0100

    Git init

---
+++ Newcomm/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Newcomm
+//
+//  Created by Klemen Zagar on 26. 03. 14.
+//  Copyright (c) 2014 Honire d.o.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ NewcommTests/NewcommTests.m
@@ -0,0 +1,34 @@
+//
+//  NewcommTests.m
+//  NewcommTests
+//
+//  Created by Klemen Zagar on 26. 03. 14.
+//  Copyright (c) 2014 Honire d.o.o. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewcommTests : XCTestCase
+
+@end
+
+@implementation NewcommTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
